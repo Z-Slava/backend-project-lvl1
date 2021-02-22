0200cd4f8a0a9bec411491c50e3ae279992ecc11
Z-Slava <zhechievviacheslav@gmail.com>
2021-02-22 19:23:02 +0200

    Feature: step 3 passed

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from "readline-sync";
+
+const askNameAndShowGreetMessage = () => {
+    const name = readlineSync.question("May I have your name? ");
+    console.log(`Hello, ${name}!`);
+};
+
+export default askNameAndShowGreetMessage;
