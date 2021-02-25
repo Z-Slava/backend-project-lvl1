65a8fe0991d63a689818581466159fc1a84f8c2d
Z-Slava <zhechievviacheslav@gmail.com>
2021-02-25 13:42:47 +0200

    Fix: remove console.log from bin file

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,22 @@
 import readlineSync from 'readline-sync';
 
-const askNameAndShowGreetMessage = () => {
+export const showWelcomeMessage = () => {
+  console.log('Welcome to the Brain Games!');
+};
+
+export const askUserName = () => {
   const name = readlineSync.question('May I have your name? ');
+  return name;
+};
+
+export const greetUserByName = (name) => {
   console.log(`Hello, ${name}!`);
+};
+
+const askNameAndShowGreetMessage = () => {
+  showWelcomeMessage();
+  const name = askUserName();
+  greetUserByName(name);
 };
 
 export default askNameAndShowGreetMessage;
