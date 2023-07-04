abeb8cc71c65c5f46d558c78f4a393b9fd797703
Vyacheslav <ZhechievViacheslav@gmail.com>
2023-07-04 19:21:34 +0300

    feat: add introduction question

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import { requestUserName } from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
+const userName = requestUserName();
+console.log(`Hello, ${userName}!`);
