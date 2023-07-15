3fab2fd4cb7393ba5ad8655a608512c7972cd714
Vyacheslav <ZhechievViacheslav@gmail.com>
2023-07-15 06:26:32 +0300

    fix: prefer default export

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import * as base from './games/base.js';
+import runGame from './games/base.js';
 import * as evenGame from './games/even.js';
 import * as calcGame from './games/calc.js';
 import * as gcdGame from './games/gcd.js';
 import * as primeGame from './games/prime.js';
 import * as progressionGame from './games/progression.js';
 
-export const runIntroGame = () => base.runGame();
-export const runEvenGame = () => base.runGame(evenGame.RULES, evenGame.getQuestionAndAnswer);
-export const runCalcGame = () => base.runGame(calcGame.RULES, calcGame.getQuestionAndAnswer);
-export const runGcdGame = () => base.runGame(gcdGame.RULES, gcdGame.getQuestionAndAnswer);
-export const runPrimeGame = () => base.runGame(primeGame.RULES, primeGame.getQuestionAndAnswer);
-export const runProgressionGame = () => base.runGame(
+export const runIntroGame = () => runGame();
+export const runEvenGame = () => runGame(evenGame.RULES, evenGame.getQuestionAndAnswer);
+export const runCalcGame = () => runGame(calcGame.RULES, calcGame.getQuestionAndAnswer);
+export const runGcdGame = () => runGame(gcdGame.RULES, gcdGame.getQuestionAndAnswer);
+export const runPrimeGame = () => runGame(primeGame.RULES, primeGame.getQuestionAndAnswer);
+export const runProgressionGame = () => runGame(
   progressionGame.RULES,
   progressionGame.getQuestionAndAnswer,
 );
